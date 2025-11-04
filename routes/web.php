f13e7b0774ddabac04ece9c874d18dc70ed69b90
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-11-04 12:06:35 +0100

    Welcome to index

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Index', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -24,9 +24,22 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/index', fn () => Inertia::render('index'))->name('index');
-
 
 // Register Page
 Route::post('/register', [UserController::class, 'register'])->name('user.register');
+
+
+/* Vue Pages Stedin  */
+
+// Nav Project
+Route::get('/project', fn() => Inertia::render('Project'))->name('project');
+
+// Nav Product
+Route::get('/product', fn() => Inertia::render('Product'))->name('product');
+
+// Nav Gebruikers
+Route::get('/gebruikers', fn() => Inertia::render('Gebruikers'))->name('gebruikers');
+
+// Nav Leveranciers
+Route::get('/leveranciers', fn() => Inertia::render('Leveranciers'))->name('leveranciers');
 
