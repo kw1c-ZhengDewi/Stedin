bc6847d059cdcaedaf9ed27961a16d04e8116755
100902glr <162437257+100902glr@users.noreply.github.com>
2025-11-10 13:29:00 +0100

    welcome

--- resources/js/Pages/WelcomePage.vue
+++ resources/js/Pages/WelcomePage.vue
@@ -8,3 +8,11 @@
 
 </template>
 
+<style scoped>
+body {
+    background-image: url('https://www.lucversleijen.nl/wp-content/uploads/stedin_default.jpg') ;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+</style>
