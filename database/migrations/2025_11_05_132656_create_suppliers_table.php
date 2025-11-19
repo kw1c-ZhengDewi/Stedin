dd788486d7edb535b4a4dea8c3deb6547baec917
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-11-19 14:29:10 +0100

    Nieuw Kolom aanmaken (Omschrijving)

--- database/migrations/2025_11_05_132656_create_suppliers_table.php
+++ database/migrations/2025_11_05_132656_create_suppliers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('email')->unique()->lowercase();
+            $table->text('supplier_description')->nullable();
             $table->string('phone_country_code', 5)->nullable();
             $table->string('phone_number');
             $table->unique(['phone_country_code', 'phone_number']); // Prevents duplicate full numbers
